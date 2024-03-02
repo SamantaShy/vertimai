70929bc18ee708b14f64b931c88fd8b0a393fc4c
Deliuxas <osvaldassarkus@gmail.com>
2024-03-02 14:58:32 +0200

    ak47_qb_anklemonitor

---
+++ en.lua
@@ -0,0 +1,21 @@
+Locales['en'] = {
+    ['noplayer'] = '~r~No player nearby!',
+    ['installing'] = 'Installing Ankle Monitor...',
+    ['removed'] = '~g~You removed then Ankle Monitor.',
+    ['removing'] = 'Removing Ankle Monitor...',
+    ['notfound'] = '~r~No Ankle Monitor attached!',
+    ['not_allowed'] = '~r~You are not allowed to use this item!',
+    ['already'] = '~r~A tracker is already attached with this person!',
+    ['gps_required'] = '~r~You don\'t have any Ankle Monitor!',
+    ['cutter_required'] = '~r~You don\'t have any Bolt Cutter!',
+    ['installed'] = '~g~Ankle Monitor installed.',
+    ['cancel'] = 'Press [E] to cancel',
+    ['cancelled'] = '~r~Task cancelled!',
+    ['alert'] = '~y~[Monitor Alert]~s~ : ~b~%s~s~ is trying to break the tracker!',
+    ['alertblip'] = '[Alert] : %s',
+    ['failed'] = '~r~You failed to break the tracker!',
+    ['time_should'] = "Time should be %s to %s minutes",
+    ['number_error'] = "Should be a number!",
+    ['set_time'] = "Set time (in minute)!",
+
+}
